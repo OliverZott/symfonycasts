ef07c44fa7e683df98d9ef1e7867e7d001c184b8
OliverZott <zott_oliver@web.de>
2020-02-22 09:52:27 +0100

    Course02-Chapter01: removed head, footer; removed get_ function; put foreach example in a function

--- index.php
+++ index.php
@@ -40,11 +40,15 @@ $walker2 = 'Tiger';
 $walker3 = 'Jay';
 $dogWalkers = [$walker1, $walker2, $walker3];
 
-foreach($dogWalkers as $walker) {
-    echo '<h3>';
-    echo $walker . '<br>';
-    echo '<button>Schedule me</button>';
-    echo '</h3>';
+
+// foreach example !
+function for_each_example($var_array) {
+    foreach($var_array as $walker) {
+        echo '<h3>';
+        echo $walker . '<br>';
+        echo '<button>Schedule me</button>';
+        echo '</h3>';
+    }
 }
 
 
@@ -80,16 +84,6 @@ var_dump($pets[1]['breed']).die();
 */
 
 
-// Functions
-function get_pets(){
-    // JSON, Files and Boolean
-    $petsJson = file_get_contents('./data/pets.json');
-    // associate array instead of object
-    // $petsArray = json_decode($petsJson, true);
-
-    return json_decode($petsJson, true);
-}
-
 $petsArray = get_pets();
 
 $messageVariable = ucwords('!message Variable');
@@ -101,78 +95,7 @@ file_put_contents($fileName, 'test2 test2', FILE_APPEND );
 
 ?>
 
-<!DOCTYPE html>
-<!--
-
-source: https://symfonycasts.com/screencast/php-ep1/arrays2#play
-
-bootstrap: https://www.w3schools.com/bootstrap/bootstrap_jumbotron_header.asp
-
--->
-
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="favicon.ico">
-
-    <title>AirPupNMeow.com: All the love, none of the crap!</title>
-
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-
-    <div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">AirPupNMeow</a>
-            </div>
-            <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li class="divider"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="navbar-form navbar-right">
-                    <div class="form-group">
-                        <input type="text" placeholder="Email" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" placeholder="Password" class="form-control">
-                    </div>
-                    <button type="submit" class="btn btn-success">Sign in</button>
-                </form>
-            </div>
-            <!--/.navbar-collapse -->
-        </div>
-    </div>
+<?php require 'layout/header.html' ?>
 
 
     <div class="jumbotron">
@@ -190,7 +113,9 @@ bootstrap: https://www.w3schools.com/bootstrap/bootstrap_jumbotron_header.asp
 
     <div class="container">
         <div class="row">
+
             <!-- close php to write inside html instead php -->
+
             <?php foreach ($petsArray as $cutePet) { ?>
             <div class="col-lg-3 pet-list-item">
                 <h2><?php echo $cutePet['name']; ?></h2>
@@ -223,7 +148,10 @@ bootstrap: https://www.w3schools.com/bootstrap/bootstrap_jumbotron_header.asp
         </div>
 
         <div class="row">
-        <?php
+
+            <!-- using php without closing -> echo needed (inconvenient) -->
+
+            <?php
             foreach($pets as $pet){
                 echo '<div class="col-lg-2">';
                 echo '<h2>';
@@ -239,13 +167,4 @@ bootstrap: https://www.w3schools.com/bootstrap/bootstrap_jumbotron_header.asp
 
         <hr>
 
-        <footer>
-            <p>&copy; AirPupNMeow.com</p>
-        </footer>
-    </div>
-    <!-- /container -->
-
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-</body>
-</html>
+<?php require 'layout/footer.html' ?>
