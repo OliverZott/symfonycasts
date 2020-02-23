d8b1bd0092ed8701e320ad59cc251c7b15e236ed
OliverZott <zott_oliver@web.de>
2020-02-23 16:52:25 +0100

    Course02-Chapter03: GET, POST, SuperGlobales ($_POST, $_SERVER)

--- pets_new.php
+++ pets_new.php
@@ -1,5 +1,37 @@
 <?php require 'layout/header.html' ?>
 
+<?php
+// POST is a super variable -> always available
+// https://www.php.net/manual/de/language.variables.superglobals.php
+
+var_dump($_POST);
+
+$name = $_POST["name"];
+var_dump($name).die;
+
+if (isset($_POST['name'])) {
+    $name = $_POST["name"];
+} else {
+    $name = '';
+}
+if (isset($_POST['breed'])) {
+    $breed = $_POST["breed"];
+} else {
+    $breed = '';
+}
+if (isset($_POST['weight'])) {
+    $weight= $_POST["weight"];
+} else {
+    $weight = '';
+}
+if (isset($_POST['bio'])) {
+    $bio = $_POST["bio"];
+} else {
+    $bio= 'empty bio';
+}
+
+?>
+
 <div class="container">
     <div class="row">
         <div class="col-xs-6">
