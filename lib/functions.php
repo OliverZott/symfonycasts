940da6389fb66a77619eef955e2bf7c3baed582e
OliverZott <zott_oliver@web.de>
2020-02-24 21:07:03 +0100

    added function comment

--- lib/functions.php
+++ lib/functions.php
@@ -1,7 +1,9 @@
 <?php
 
-function get_pets()
-{
+function get_pets() {
+    /**
+     * Returns pets.json as an associative array
+     */
     $petsJson = file_get_contents('./data/pets.json');
 
     return json_decode($petsJson, true);
