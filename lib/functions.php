3039dd29bf6041775bc62c43ecc08276bb736c9b
OliverZott <zott_oliver@web.de>
2020-03-01 00:51:35 +0100

    new function - save_pets

--- lib/functions.php
+++ lib/functions.php
@@ -5,6 +5,16 @@ function get_pets() {
      * Returns pets.json as an associative array
      */
     $petsJson = file_get_contents('./data/pets.json');
-
     return json_decode($petsJson, true);
+}
+
+function save_pets($petsToSave) {
+    /**
+     * Function to save pet-array to a json file!
+     *
+     * @parameter: array (containing pet)
+     */
+    $pets_json = json_encode($petsToSave, JSON_PRETTY_PRINT);
+    $filename = './data/pets.json';
+    file_put_contents($filename, $pets_json);  //var_dump($name, $breed, $weight, $bio);
 }
